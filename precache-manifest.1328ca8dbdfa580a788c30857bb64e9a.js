2bcc6c785a337d59844190df8c45779c8e6de290
danielafrolich <danielafrolich@gmail.com>
2019-11-29 09:52:02 +0100

    Updates

---
+++ precache-manifest.1328ca8dbdfa580a788c30857bb64e9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03caeab26d3c3c84c0f78e3e383bc305",
+    "url": "/FrolichConsulting/index.html"
+  },
+  {
+    "revision": "d699221073f2193d6713",
+    "url": "/FrolichConsulting/static/css/main.339b7319.chunk.css"
+  },
+  {
+    "revision": "05347abd8480d3edd0d9",
+    "url": "/FrolichConsulting/static/js/2.03e03fa6.chunk.js"
+  },
+  {
+    "revision": "d699221073f2193d6713",
+    "url": "/FrolichConsulting/static/js/main.67411091.chunk.js"
+  },
+  {
+    "revision": "e02e402192e260c5e09d",
+    "url": "/FrolichConsulting/static/js/runtime-main.eedf86b4.js"
+  },
+  {
+    "revision": "1e0b19a05dcb71b48171b0a3e558c1b9",
+    "url": "/FrolichConsulting/static/media/background.1e0b19a0.png"
+  },
+  {
+    "revision": "03bad0f9a170ac75f29cef9f6d1ba341",
+    "url": "/FrolichConsulting/static/media/briefcase.03bad0f9.svg"
+  },
+  {
+    "revision": "1581aaf8efba389cb4b5b026905c0bed",
+    "url": "/FrolichConsulting/static/media/code.1581aaf8.svg"
+  },
+  {
+    "revision": "ba31da26ad13482904c2ec5fb92a1695",
+    "url": "/FrolichConsulting/static/media/degree.ba31da26.svg"
+  },
+  {
+    "revision": "3526fc5a0974f1bbcd8eb9fd73c1fdf2",
+    "url": "/FrolichConsulting/static/media/logo.3526fc5a.svg"
+  },
+  {
+    "revision": "c3f20668a2835705cdbc6125d9be8b49",
+    "url": "/FrolichConsulting/static/media/profile_pic.c3f20668.png"
+  },
+  {
+    "revision": "c5d9df01b4f5bfbcdf08ffe9fc536202",
+    "url": "/FrolichConsulting/static/media/resume.c5d9df01.svg"
+  }
+]);
