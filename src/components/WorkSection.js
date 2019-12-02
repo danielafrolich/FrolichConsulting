4a11ccd64688a9d005ab722adea49e1c134f8f8a
danielafrolich <danielafrolich@gmail.com>
2019-12-02 14:42:23 +0100

    Responsive cards

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import MediaControlCard from "../components/Cards/Cards"
+import Card from "./Card/Card"
+import ai from './../images/ai.png'
+import volvo from './../images/volvo.png'
+import capGemini from './../images/cap_gemini.png'
+import telia from './../images/telia.png'
 
 class WorkSection extends React.Component{
   render(){
@@ -8,7 +12,34 @@ class WorkSection extends React.Component{
         <div className="centered-content">
           <h1>Work</h1>
           <p>Here is a selection of my work experience. For more details see my resume.</p>
-          <MediaControlCard />
+          <Card
+          cardMedia={ai}
+          alt="Artificial Intelligence"
+          cardHeader="Master Thesis in AI"
+          cardSubtitle="Chalmers & Ericsson"
+          cardParagraph="The thesis examined the possibility of using machine learning algorithms to predict possible overloads in a specific node in a cellular network. The thesis resulted in a proof of concept which showed that machine learning could be used to predict overloads with good results." >
+          </Card>
+          <Card
+          cardMedia={telia}
+          alt="Telia"
+          cardHeader="Software developer"
+          cardSubtitle="Telia"
+          cardParagraph="Lorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum bla." >
+          </Card>
+          <Card
+          cardMedia={capGemini}
+          alt="Cap Gemini"
+          cardHeader="Software developer"
+          cardSubtitle="Cap Gemini"
+          cardParagraph="I started at Capgemini as a graduate in their Ignite program and graduated after 1,5 of training and courses. Thereafter I continued to work as a consultant for their clients developing my programming skills in, for example, Java and Kotlin." >
+          </Card>
+          <Card
+          cardMedia={volvo}
+          alt="Volvo"
+          cardHeader="Software developer"
+          cardSubtitle="Volvo Group"
+          cardParagraph="Worked two summers at Volvo Car Group were my college and I explored and developed methods to automate the audio testing of the speakers in Volvo cars using Python. We  were able to develop three separate programs, written with Python, to help the audio testing group at Volvo with the automation of testing." >
+          </Card>
         </div>
       </section>
     );
