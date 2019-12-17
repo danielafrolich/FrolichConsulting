f502886ecee47adaa345f58bcbdfffd5c8816392
danielafrolich <danielafrolich@gmail.com>
2019-12-17 09:56:20 +0100

    Fixed bug.

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -12,16 +12,13 @@ class WorkSection extends React.Component{
   render(){
     return(
       <section className="work" role="region" aria-labelledby="workRegion" aria-label="Isabelle Frölich work experience.">
-        <div hidden>
-          <span id="new-window">Opens in a new widow</span>
-        </div>
         <div className="centered-content">
-          <h1  id="workRegion">Work</h1>
-          <p>Here is a selection of my work experience. For more details see my <a target="_blank" aria-describedby="new-window" rel="noopener noreferrer" href={resumePdf}>resume</a>.</p>
+          <h1>Work</h1>
+          <h1 id="workRegion">Work</h1>
+          <p>Here is a selection of my work experience. For more details see my <a target="_blank" rel="noopener noreferrer" href={resumePdf}>resume</a>.</p>
           <LinkCard
           cardMedia={ai}
           alt="Artificial Intelligence"
-          altLink="New window"
           cardHeader="Master Thesis in AI"
           cardSubtitle="Chalmers & Ericsson"
           cardParagraph="The thesis examined the possibility of using machine learning algorithms to predict possible overloads in a specific node in a cellular network. The thesis resulted in a proof of concept which showed that machine learning could be used to predict overloads with good results."
@@ -35,21 +32,22 @@ class WorkSection extends React.Component{
           alt="Telia"
           cardHeader="Software developer"
           cardSubtitle="Telia"
-          cardParagraph="Started at Telia 2018 as a backend developer working with their aggregated streaming service. Here I learned to work with microservices that uses a wide variety of technologies and frameworks to optimize performance. I also gained 2 more years experience with programming in Kotlin and Java." >
+          cardParagraph="Lorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum blaLorem ipsum bla." >
           </Card>
           <Card
           cardMedia={capGemini}
+          alt="Cap Gemini"
           alt="Capgemini"
           cardHeader="Software developer"
-          cardSubtitle="Capgemini"
-          cardParagraph="I started at Capgemini as a graduate in their Ignite program and graduated after 1,5 years of training and courses. Thereafter I continued to work as a consultant for their clients developing my programming skills in, for example, Java and Kotlin." >
+          cardSubtitle="Cap Gemini"
+          cardParagraph="I started at Capgemini as a graduate in their Ignite program and graduated after 1,5 of training and courses. Thereafter I continued to work as a consultant for their clients developing my programming skills in, for example, Java and Kotlin." >
           </Card>
           <Card
           cardMedia={volvo}
           alt="Volvo"
           cardHeader="Software developer"
-          cardSubtitle="Volvo Car Group"
-          cardParagraph="Worked two summers at Volvo Car Group were my colleague and I explored and developed methods to automate the audio testing of the speakers in Volvo cars using Python. We  were able to develop three separate programs, written with Python, to help the audio testing group at Volvo with the automation of testing." >
+          cardSubtitle="Volvo Group"
+          cardParagraph="Worked two summers at Volvo Car Group were my college and I explored and developed methods to automate the audio testing of the speakers in Volvo cars using Python. We  were able to develop three separate programs, written with Python, to help the audio testing group at Volvo with the automation of testing." >
           </Card>
         </div>
       </section>
